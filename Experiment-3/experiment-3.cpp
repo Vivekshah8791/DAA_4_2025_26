10a2cbe9132f725526b0d601bba1a5c3cf2c7ed7
Vivek shah <vivekshah6751@gmail.com>
2026-02-01 20:38:58 +0530

    Add files via upload

---
+++ Experiment-3/experiment-3.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<char> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    unordered_map<int, int> mp;
+    int sum = 0;
+    int maximum = 0;
+
+    mp[0] = -1;  
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == 'P')
+            sum += 1;
+        else if (arr[i] == 'A')
+            sum -= 1;
+
+        if (mp.find(sum) != mp.end()) {
+            maximum = max(maximum, i - mp[sum]);
+        } else {
+            mp[sum] = i;
+        }
+    }
+
+    cout << maximum << endl;
+    return 0;
+}
